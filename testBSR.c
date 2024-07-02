789c7b16e38dc77822e54e559d5c0d08e5095170
kimkratos <tykratos@itcom888.com>
2024-07-02 17:08:21 +0800

    Update testBSR.c

--- testBSR.c
+++ testBSR.c
@@ -7,11 +7,9 @@ unsigned int count_bits_clzl(unsigned long x) {
     return 8 * sizeof(long) - __builtin_clzl(x);
 }
 
-// 使用 BSR 方法，移到内联函数中
-inline unsigned int count_bits_bsr(unsigned long x) {
-    unsigned long index;
-    __asm__("bsr %1, %0" : "=r" (index) : "r" (x));
-    return index + 1;
+// 使用 __builtin_ffs 方法
+unsigned int count_bits_ffs(unsigned long x) {
+    return __builtin_clzl(x - 1) + 1;
 }
 
 int main() {
@@ -32,14 +30,14 @@ int main() {
     elapsed_ns = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
     printf("Using __builtin_clzl: %lld ns\n", elapsed_ns);
 
-    // 使用 BSR 方法
+    // 使用 __builtin_ffs 方法
     clock_gettime(CLOCK_MONOTONIC, &start);
     for (int i = 0; i < iterations; ++i) {
-        bits = count_bits_bsr(num);
+        bits = count_bits_ffs(num);
     }
     clock_gettime(CLOCK_MONOTONIC, &end);
     elapsed_ns = (end.tv_sec - start.tv_sec) * 1000000000LL + (end.tv_nsec - start.tv_nsec);
-    printf("Using BSR: %lld ns\n", elapsed_ns);
+    printf("Using __builtin_ffs: %lld ns\n", elapsed_ns);
 
     return 0;
 }
